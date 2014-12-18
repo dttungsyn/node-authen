7c3470c3c9edcfa221f502e664540c5d764af90e
Dang Tung <dttung1412@gmail.com>
2014-12-18 11:29:10 +0900

    change database server

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://admin:12345@172.27.197.40:27017/fpt_time?authSource=admin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://admin:12345@172.27.197.40:27017/fpt_time?authSource=admin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://172.27.187.28:27017/fpt_time'
 
 };
