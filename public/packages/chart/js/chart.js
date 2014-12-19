27d90041cbed13bb4d3931bac2229642cd971981
Vu Quang Dung <vuquangdung3d@gmail.com>
2014-12-19 18:05:47 +0900

    chart

--- public/packages/chart/js/chart.js
+++ public/packages/chart/js/chart.js
@@ -12,98 +12,109 @@ angular.module('todoController').controller(
 					var fpttime = window.fpttime || {};
 					window.fpttime = fpttime;
 
-					//d3.json('stackedAreaData.json', function(dataTest) {
-					nv.addGraph(function() {
+					/*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
+					nv
+							.addGraph(function() {
+								var chart = nv.models.lineChart().margin({
+									left : 100
+								}) //Adjust chart margins to give the x-axis some breathing room.
+								.useInteractiveGuideline(true) //We want nice looking tooltips and a guideline!
+								.transitionDuration(350) //how fast do you want the lines to transition?
+								.showLegend(true) //Show the legend, allowing users to turn on/off line series.
+								.showYAxis(true) //Show the y-axis
+								.showXAxis(true) //Show the x-axis
+								;
 
-						var chart = nv.models.stackedAreaChart().margin({
-							right : 100
-						}).x(function(d) {
-							return d[0];
-						}) //We can modify the data accessor functions...
-						.y(function(d) {
-							return d[1];
-						}) //...in case your data is formatted differently.
-						.useInteractiveGuideline(true) //Tooltips which show all data points. Very nice!
-						.rightAlignYAxis(true) //Let's move the y-axis to the right side.
-						.transitionDuration(500).showControls(true) //Allow user to choose 'Stacked', 'Stream', 'Expanded' mode.
-						.clipEdge(true);
+								chart.xAxis //Chart x-axis settings
+								.axisLabel('日').tickFormat(d3.format(',r'));
+								
+								chart.yAxis //Chart y-axis settings
+								.axisLabel('時間').tickFormat(function(d){
+									var hour = Math.floor( d );
+									var minute = Math.floor((d-hour)*60);
+									minute = minute > 9 ? "" + minute: "0" + minute;
+									return hour + ":" + minute;
+								});
 
-						//Format x-axis labels with custom function.
-						chart.xAxis.tickFormat(function(d) {
-							//return d3.time.format('%x')(new Date(d));
-							return d+"日";
-						});
-
-						chart.yAxis.tickFormat(d3.format(',.2f'));
-
-						var timeData = $scope.$parent.timeDatas.data;
-						var yearMonth = $scope.$parent.monthstr;
-						d3.select('#chart svg').datum(
-								exampleData(timeData, yearMonth)).call(chart);
-
-						nv.utils.windowResize(chart.update);
-
-						$scope.$watch(function() {
-							return $scope.$parent.timeDatas.data;
-						}, function() {
-
-							console.log("Time data changed!");
-
-							//update chart
-							setTimeout(function() {
 								var timeData = $scope.$parent.timeDatas.data;
 								var yearMonth = $scope.$parent.monthstr;
-								d3.select('#mychart svg').datum(
-										exampleData(timeData, yearMonth)).call(
-										chart);
 
-							}, 500);
+								d3.select('#chart svg') //Select the <svg> element you want to render the chart in.   
+								.datum(exampleData(timeData, yearMonth)) //Populate the <svg> element with chart data...
+								.call(chart); //Finally, render the chart!
 
-						});
+								//Update the chart when window resizes.
+								nv.utils.windowResize(function() {
+									chart.update()
+								});
+								
+								$scope.$watch(function() {
+									return $scope.$parent.timeDatas.data;
+								}, function() {
 
-						fpttime.chart = chart;
+									console.log("Time data changed!");
 
-						return chart;
-					});
+									//update chart
+									setTimeout(function() {
+										var timeData = $scope.$parent.timeDatas.data;
+										var yearMonth = $scope.$parent.monthstr;
+										d3.select('#mychart svg').datum(
+												exampleData(timeData, yearMonth)).call(
+												chart);
+
+									}, 500);
+
+								});
+								
+								return chart;
+							});
 
 				} ])
 
 //Each bar represents a single discrete quantity.
 function exampleData(timeData, yearMonth) {
-	console.log(yearMonth);
+
 	console.log(timeData);
-	return [
-			{
-				"key" : "出社",
-				"values" : [ [ 1, 9 ], [ 2, 9 ],
-						[ 3, 9 ], [ 4, 9 ],
-						[ 5, 9 ], [ 6, 0 ],
-						[ 7, 0 ], [ 8, 9 ],
-						[ 9, 9 ], [ 10, 9 ],
-						[ 11, 9 ], [ 12, 9 ] ],
-						"color": "#ffffff"
-			},
-
-			{
-				"key" : "出社",
-				"values" : [ [ 1, 9 ], [ 2, 9 ],
-						[ 3, 9 ], [ 4, 9 ],
-						[ 5, 9 ], [ 6, 0 ],
-						[ 7, 0 ], [ 8, 9 ],
-						[ 9, 9 ], [ 10, 9 ],
-						[ 11, 9 ], [ 12, 9 ] ]
-			},
-
-			{
-				"key" : "出社",
-				"values" : [ [ 1, 1 ], [ 2, 0 ],
-						[ 3, 2 ], [ 4, 3 ],
-						[ 5, 1 ], [ 6, 0 ],
-						[ 7, 0 ], [ 8, 2 ],
-						[ 9, 2 ], [ 10, 0 ],
-						[ 11, 0 ], [ 12, 1 ] ]
-			}
-
-	];
+	
+	var day = 0;
+	var time;
+	
+	var timeIn = [];
+	var timeOut = [];
+	var temp = [];
+	
+	for(day=0; day<timeData.length; day++){
+		time = timeData[day];
+		if(time[3] == "") continue;
+		temp = time[3].split(":");
+		timeIn.push({x:day+1,y:parseInt(temp[0]) + parseInt(temp[1])/60});
+		temp = time[4].split(":");
+		timeOut.push({x:day+1,y:parseInt(temp[0]) + parseInt(temp[1])/60});
+	}
+	return [{values:timeIn,key:'出社',color:'blue'},{values:timeOut,key:'退社',color:'red'}];
+//	return [
+//	        {
+//	          values: [{x:1,y:9.5},{x:2,y:10},{x:3,y:9},{x:4,y:9.5},{x:5,y:9},{x:6,y:10},{x:7,y:9},{x:8,y:10},{x:9,y:9},{x:10,y:10},{x:11,y:9},{x:12,y:10},{x:13,y:9},{x:14,y:10}],
+//	          key: 'in',
+//	          color: 'blue'
+//	        },
+//	        {
+//	          values: [{x:1,y:19},{x:2,y:20},{x:3,y:19},{x:4,y:18},{x:5,y:15},{x:6,y:17},{x:7,y:19},{x:8,y:20},{x:9,y:19},{x:10,y:18},{x:11,y:19},{x:12,y:20},{x:13,y:22},{x:14,y:21}],
+//	          key: 'out',
+//	          color: 'red'
+//	        },
+//	        {
+//	          values: [{x:1,y:9},{x:2,y:9},{x:3,y:9},{x:4,y:7.5},{x:5,y:8},{x:6,y:6},{x:7,y:9},{x:8,y:9},{x:9,y:9},{x:10,y:7},{x:11,y:9},{x:12,y:9},{x:13,y:12.5},{x:14,y:10}],
+//	          key: 'in work',
+//	          color: 'yellow',
+//	          area : true
+//	        },
+//	        {
+//	          values: [{x:1,y:1},{x:2,y:1},{x:3,y:1},{x:4,y:0},{x:5,y:0},{x:6,y:0},{x:7,y:1},{x:8,y:1},{x:9,y:1},{x:10,y:0},{x:11,y:1},{x:12,y:1},{x:13,y:4.5},{x:14,y:2}],
+//	          key: 'ot',
+//	          color: 'green',
+//	          area : true
+//	        }
+//	      ];
 
 }
