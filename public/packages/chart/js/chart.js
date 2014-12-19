8ec7979b544c8fad516382f083ca650ec136fc97
Vu Quang Dung <vuquangdung3d@gmail.com>
2014-12-19 16:21:59 +0900

    chart

--- public/packages/chart/js/chart.js
+++ public/packages/chart/js/chart.js
@@ -3,91 +3,107 @@
  * parent: mainController
  */
 
-angular.module('todoController')
-	.controller('chartController', ['$scope', function($scope) {
-		
-		var fpttime = window.fpttime || {};
-		window.fpttime = fpttime;
-		
-		nv.addGraph(function() {
-		  var chart = nv.models.discreteBarChart()
-		      .x(function(d) { return d.label ;})    //Specify the data accessors.
-		      .y(function(d) { return d.value ;})
-		      .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
-		      .tooltips(false)        //Don't show tooltips
-		      .showValues(true)       //...instead, show the bar value right on top of each bar.
-		      .transitionDuration(350)
-		      ;
-		
-		  d3.select('#mychart svg')
-		      .datum(exampleData())
-		      .call(chart);
-		
-		  nv.utils.windowResize(chart.update);
-		  
-		  $scope.$watch(function() { return $scope.$parent.timeDatas.data ; },
-		      function() {
-		      	var timedata = $scope.$parent.timeDatas.data;
-		      	console.log( "Time data changed!" );
-		      	
-		      	//update chart
-		      	setTimeout(function(){
-		      		/*
-					  d3.select('#mychart svg')
-										.datum(exampleData())
-										.call(chart);*/
-					  
-		      	}, 500);
-		      	
-		      }
-		  );
-		  
-		  fpttime.chart = chart;
-		
-		  return chart;
-		});
-	}])
-	
+angular.module('todoController').controller(
+		'chartController',
+		[
+				'$scope',
+				function($scope) {
+
+					var fpttime = window.fpttime || {};
+					window.fpttime = fpttime;
+
+					//d3.json('stackedAreaData.json', function(dataTest) {
+					nv.addGraph(function() {
+
+						var chart = nv.models.stackedAreaChart().margin({
+							right : 100
+						}).x(function(d) {
+							return d[0];
+						}) //We can modify the data accessor functions...
+						.y(function(d) {
+							return d[1];
+						}) //...in case your data is formatted differently.
+						.useInteractiveGuideline(true) //Tooltips which show all data points. Very nice!
+						.rightAlignYAxis(true) //Let's move the y-axis to the right side.
+						.transitionDuration(500).showControls(true) //Allow user to choose 'Stacked', 'Stream', 'Expanded' mode.
+						.clipEdge(true);
+
+						//Format x-axis labels with custom function.
+						chart.xAxis.tickFormat(function(d) {
+							//return d3.time.format('%x')(new Date(d));
+							return d+"日";
+						});
+
+						chart.yAxis.tickFormat(d3.format(',.2f'));
+
+						var timeData = $scope.$parent.timeDatas.data;
+						var yearMonth = $scope.$parent.monthstr;
+						d3.select('#chart svg').datum(
+								exampleData(timeData, yearMonth)).call(chart);
+
+						nv.utils.windowResize(chart.update);
+
+						$scope.$watch(function() {
+							return $scope.$parent.timeDatas.data;
+						}, function() {
+
+							console.log("Time data changed!");
+
+							//update chart
+							setTimeout(function() {
+								var timeData = $scope.$parent.timeDatas.data;
+								var yearMonth = $scope.$parent.monthstr;
+								d3.select('#mychart svg').datum(
+										exampleData(timeData, yearMonth)).call(
+										chart);
+
+							}, 500);
+
+						});
+
+						fpttime.chart = chart;
+
+						return chart;
+					});
+
+				} ])
+
 //Each bar represents a single discrete quantity.
-function exampleData() {
- return  [ 
-    {
-      key: "Cumulative Return",
-      values: [
-        { 
-          "label" : "A Label" ,
-          "value" : -29.765957771107
-        } , 
-        { 
-          "label" : "B Label" , 
-          "value" : 0
-        } , 
-        { 
-          "label" : "C Label" , 
-          "value" : 32.807804682612
-        } , 
-        { 
-          "label" : "D Label" , 
-          "value" : 196.45946739256
-        } , 
-        { 
-          "label" : "E Label" ,
-          "value" : 0.19434030906893
-        } , 
-        { 
-          "label" : "F Label" , 
-          "value" : -98.079782601442
-        } , 
-        { 
-          "label" : "G Label" , 
-          "value" : -13.925743130903
-        } , 
-        { 
-          "label" : "H Label" , 
-          "value" : -5.1387322875705
-        }
-      ]
-    }
-  ];
+function exampleData(timeData, yearMonth) {
+	console.log(yearMonth);
+	console.log(timeData);
+	return [
+			{
+				"key" : "出社",
+				"values" : [ [ 1, 9 ], [ 2, 9 ],
+						[ 3, 9 ], [ 4, 9 ],
+						[ 5, 9 ], [ 6, 0 ],
+						[ 7, 0 ], [ 8, 9 ],
+						[ 9, 9 ], [ 10, 9 ],
+						[ 11, 9 ], [ 12, 9 ] ],
+						"color": "#ffffff"
+			},
+
+			{
+				"key" : "出社",
+				"values" : [ [ 1, 9 ], [ 2, 9 ],
+						[ 3, 9 ], [ 4, 9 ],
+						[ 5, 9 ], [ 6, 0 ],
+						[ 7, 0 ], [ 8, 9 ],
+						[ 9, 9 ], [ 10, 9 ],
+						[ 11, 9 ], [ 12, 9 ] ]
+			},
+
+			{
+				"key" : "出社",
+				"values" : [ [ 1, 1 ], [ 2, 0 ],
+						[ 3, 2 ], [ 4, 3 ],
+						[ 5, 1 ], [ 6, 0 ],
+						[ 7, 0 ], [ 8, 2 ],
+						[ 9, 2 ], [ 10, 0 ],
+						[ 11, 0 ], [ 12, 1 ] ]
+			}
+
+	];
 
 }
