a9eeade14b0e3fac177ee96e885d51f7f09f3d5b
Dang Tung <dttung1412@gmail.com>
2015-04-01 15:09:34 +0900

    abcd

--- public/packages/admin/js/controllers/admin.js
+++ public/packages/admin/js/controllers/admin.js
@@ -8,6 +8,7 @@ angular.module('adminModule', [])
 		$scope.tableData = [];
 		$scope.openUserInd = 0;
 		$scope.openUser = null;
+		var colsChange = false;
 		$scope.save = function(){
 			
 			//close modal
@@ -22,19 +23,31 @@ angular.module('adminModule', [])
 			});
 			
 			//reload / update dataTable
-			adminServices.datatable.row(ind).data(userData).draw();
-			var tr = adminServices.datatable.row(ind).nodes().to$(); 
-			tr.find("td:last-child").append( adminServices.datatable.groupIcons.clone() );
+			if ( !colsChange ){
+				adminServices.datatable.row(ind).data(userData).draw();
+				var tr = adminServices.datatable.row(ind).nodes().to$(); 
+				tr.find("td:last-child").append( adminServices.datatable.groupIcons.clone() );
+				
+				//remark change
+				tr.addClass('just-modified');
+				setTimeout(function(){
+					tr.removeClass( 'just-modified' );
+				}, 200);
+				
+			}
 			
-			//remark change
-			tr.addClass('just-modified');
-			setTimeout(function(){
-				tr.removeClass( 'just-modified' );
-			}, 200);
+			else {
+			// if columns number change
+				adminServices.destroyTable();
+				adminServices.getAndRender( $scope );
+			}
+			
 		}
 		
 		$scope.removeCustomCol = function( index ){
 			$scope.customColumns.splice( index, 1 );
+			colsChange = true;
+			
 		}
 		
 		$scope.addCustomCol = function(){
@@ -42,6 +55,7 @@ angular.module('adminModule', [])
 			if ( newCol && $scope.fixColumns.indexOf(newCol) < 0 && $scope.customColumns.indexOf(newCol) < 0 ){
 				$scope.customColumns.push( newCol );
 			}
+			colsChange = true;
 		}
 		
 		$( ".sortable" ).sortable();
--- public/packages/admin/js/directives/admin.js
+++ public/packages/admin/js/directives/admin.js
@@ -8,101 +8,10 @@ angular.module('adminModule')
 		console.log('directive run');
 		
 		function link(scope, element, attrs) {
-			
-			$http.get('/api/staffuserdata').success(function(data){
-				
-				if (!data || data.length === 0){
-					console.log( 'err' );
-					return;
-				}
-				
-				console.log( data );
-				
-				var columns = data[0].data_fields.map(function(field, ind){
-					return {data: field};
-				});
-				
-				columns.unshift( {data: "userid"}, {data: "email"} );
-				
-				//update scope
-				scope.fixColumns = ['userid', 'email'];
-				scope.customColumns = data[0].data_fields || [];
-				scope.tableData = tableData;
-				
-				//add fix data to custom data
-				var tableData = data.map( function( staff, ind ){
-					//add _id, userid, (email)
-					var obj = staff.data || {};
-					obj._id = staff._id;
-					obj.userid = staff.local.username;
-					obj.email  = staff.local.email;
-					
-					
-					for (var i = 1; i < columns.length; i ++){
-						if ( !obj[ columns[i].data ] ) obj[ columns[i].data ] = "";
-					}
-					
-					return obj;
-				});
-				
-				
-				// take the group icons inside table element
-				var groupIcons = element.find( ".group-icons" ).detach();
-				
-				//create header element 
-				var head = $("<thead><tr></tr></thead>").appendTo(element);
-				for (var i in columns){
-					var column = columns[i].data;
-					head.append( $("<th></th>").text( column ) );
-				}
-				
-				//make DataTable
-				var table = element.DataTable({
-					data: tableData,
-					columns: columns,
-					paging: false
-				});
-				table.groupIcons = groupIcons;
-				
-				//debug
-				window.tableda = table;
-				
-				// save to service
-				adminServices.datatable = table;
-				
-				// add group icons to each line
-				element.find("tbody tr td:last-child").append( groupIcons.clone() );
-				
-				// add class for table
-				element.addClass("staff-data-tbl");
-				
-				// create modal to edit
-				$('#myModal').modal({
-					show: false
-				});
-				
-				// handle icon click
-				element.find("tbody tr td:last-child").click(function(e){
-					if ( !$(e.target).hasClass('fa-edit') ) return;
-					//var _userid = $(this).closest("tr").
-					scope.openUserInd = _findIndByUserId( tableData, table.row( $(this).closest("tr") ).data().userid );
-					scope.openUser = table.row( $(this).closest("tr") ).data();
-					
-					console.log( table.row( $(this).closest("tr") ).data() );
-					
-					$('#myModal').modal('show');
-					scope.$apply();
-				});
-			})
+			adminServices.getAndRender( scope, element );
 		}
 		
-		function _findIndByUserId( tableData, userid ){
-			for (var i in tableData){
-				if (tableData[i].userid === userid) return i;
-			}
-			
-			return -1;
-		}
+		
 		
 		return {
 			link: link
